e4f136e73c4648e04e718dd820e22deaad3eab9b
Coin Chiang <myabandonment@foxmail.com>
2025-03-04 15:24:51 +0800

    Add Model User.

--- README.md
+++ README.md
@@ -6,6 +6,30 @@ Yuyi is the RESTful API backend for a generic podcast hosting platform.
 
 ### 2025-03-04
 
-- [ ] Initial project
+- [x] Initial project
 - [ ] Build database models
 
+[A Podcaster’s Guide to RSS - Apple](https://help.apple.com/itc/podcasts_connect/)
+
+User:
+
+- UserBase
+    - username
+    - nickname 
+    - email
+    - description
+- User
+    - id
+    - hash_password
+    - avatar_url
+    - createtime
+- UserUpload
+- UserPublic
+    - id
+    - avatar_url
+    - createtime
+
+Podcast:
+
+- PodcastBase:
+    - 
---
+++ app/models/user.py
@@ -0,0 +1,24 @@
+from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
+from datetime import date
+
+class UserBase(SQLModel):
+    username: str
+    nickname: str
+    email: EmailStr | None = None
+    description: str | None = None
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str
+    avatar_url: str | None = None
+    createtime: date
+
+class UserUpload(UserBase):
+    pass
+
+class UserPublic(UserBase):
+    id: int
+    avatar_url: str | None
+    createtime: date
+
